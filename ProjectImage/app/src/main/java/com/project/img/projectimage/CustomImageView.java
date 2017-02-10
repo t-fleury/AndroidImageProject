ec87607beb211dfe5c81180b83d3a12e2333e2d8
gauthier <gauthier.lamarque@gmail.com>
2017-02-10 15:35:36 +0100

    FIXing custom view

--- ProjectImage/app/src/main/java/com/project/img/projectimage/CustomImageView.java
+++ ProjectImage/app/src/main/java/com/project/img/projectimage/CustomImageView.java
@@ -3,8 +3,12 @@ package com.project.img.projectimage;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
 /**
@@ -13,25 +17,53 @@ import android.widget.ImageView;
 
 public class CustomImageView extends ImageView {
 
-    private Bitmap mBitmap;
-    private int mImageWidth;
-    private int mImageHeight;
+    private Bitmap bitmap;
+
     private static float MIN_ZOOM = 1f;
     private static float MAX_ZOOM = 5f;
 
     private float scaleFactor = 1.f;
+    private ScaleGestureDetector detector;
 
     public CustomImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        detector = new ScaleGestureDetector(getContext(), new ScaleListener());
+        bitmap = ((BitmapDrawable)getDrawable()).getBitmap();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        detector.onTouchEvent(event);
+        return true;
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        canvas.save();
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleFactor, scaleFactor, pivotPointX,
+                pivotPointY);
+        // canvas.setMatrix(matrix);
+
+        canvas.drawBitmap(
+                ((BitmapDrawable) this.getDrawable()).getBitmap(), matrix,
+                null);
+
+        canvas.restore();
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            Log.d("scale detector", "detected");
+            scaleFactor *= detector.getScaleFactor();
+            scaleFactor = Math.max(MIN_ZOOM, Math.min(scaleFactor, MAX_ZOOM));
+            invalidate();
+            return true;
+        }
     }
 }
