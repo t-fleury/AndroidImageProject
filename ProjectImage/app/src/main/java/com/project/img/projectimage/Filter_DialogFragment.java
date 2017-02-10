46588c8bdb818ab8154917f14bba2a238e71b4dc
t-fleury <fleurythomaspro@gmail.com>
2017-02-10 11:00:35 +0100

    ADD name filter(2) + ADD filter_Dialog

--- ProjectImage/app/src/main/java/com/project/img/projectimage/Filter_DialogFragment.java
+++ ProjectImage/app/src/main/java/com/project/img/projectimage/Filter_DialogFragment.java
@@ -3,6 +3,8 @@ package com.project.img.projectimage;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 /**
@@ -10,15 +12,18 @@ import android.os.Bundle;
  */
 
 public class Filter_DialogFragment extends DialogFragment {
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-       /* builder.setTitle("@string/filter")
-                .setItems(, new DialogInterface.OnClickListener() {
+       Resources res = getResources();
+       AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+       builder.setTitle(getString(R.string.filter))
+                .setItems(res.getStringArray(R.array.filter_name), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int option) {
                         dialog.dismiss();
                     }
-                });*/
+                });
         return builder.create();
     }
 }
