36e22549cfc396bceccf698889a4909824fc22da
t-fleury <fleurythomaspro@gmail.com>
2017-02-17 11:24:30 +0100

    ADD Convolution(not finished)

--- ProjectImage/app/src/main/java/com/project/img/projectimage/MainActivity.java
+++ ProjectImage/app/src/main/java/com/project/img/projectimage/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -25,7 +26,10 @@ import static com.project.img.projectimage.Filters.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap bitmap;
+    private ImageView imageView;
+    final private int sizeMatrix = 3;
+    /*private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -53,6 +57,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
+    }*/
+
+    //NOT FINISHED
+    private void convulation(){
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int[] pixels = new int[width * height];
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+        int averageR = 0, averageG = 0, averageB = 0;
+        for (int i = 0; i < width - sizeMatrix; i++) {
+            for (int j = 0; j < height - sizeMatrix; j++) {
+                for (int k = i; k < sizeMatrix; k++) {
+                    for (int l = j; l < sizeMatrix; l++) {
+                        averageR += Color.red(bitmap.getPixel(k,l));
+                        averageG += Color.green(bitmap.getPixel(k,l));
+                        averageB += Color.blue(bitmap.getPixel(k,l));
+                    }
+                }
+               pixels[i+(sizeMatrix/2) + (j+(sizeMatrix/2)) * width] =
+                       Color.rgb((averageR / (sizeMatrix*sizeMatrix))%256,
+                                 (averageG / (sizeMatrix*sizeMatrix))%256,
+                                 (averageB / (sizeMatrix*sizeMatrix))%256);
+               averageR = 0;
+               averageG = 0;
+               averageB = 0;
+            }
+        }
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.pandaria);
+        bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageBitmap(bitmap);
     }
 
     @Override
@@ -60,7 +102,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Buttons
+        Button testFilter = (Button) findViewById(R.id.testFilter);
+        testFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convulation();
+            }
+        });
+
+       /* //Buttons
         Button take = (Button) findViewById(R.id.Take);
         Button getImg = (Button) findViewById(R.id.getImg);
         Button getSaved = (Button) findViewById(R.id.getSaved);
@@ -96,9 +146,9 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, 0);
             }
-        }
+        }*/
     }
-
+    /*
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
@@ -129,5 +179,5 @@ public class MainActivity extends AppCompatActivity {
 
     public Bitmap getpicture() {
         return picture;
-    }
+    }*/
 }
