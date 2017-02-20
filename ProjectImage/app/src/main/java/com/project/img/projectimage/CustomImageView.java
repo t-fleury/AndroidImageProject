bab4459a53319cdf4ecc67fe11bc0ca3bbf2dbbe
gauthier <gauthier.lamarque@gmail.com>
2017-02-20 18:56:29 +0100

    ADD scroll mais les limites sont pas les bonnes, à fix

--- ProjectImage/app/src/main/java/com/project/img/projectimage/CustomImageView.java
+++ ProjectImage/app/src/main/java/com/project/img/projectimage/CustomImageView.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -17,10 +20,23 @@ import android.widget.ImageView;
 
 public class CustomImageView extends ImageView {
 
-    //private Bitmap bitmap;
+    private Drawable drawable;
+    private Bitmap bitmap;
+    private int limitX;
+    private int limitY;
 
     private static float MIN_ZOOM = 1f;
     private static float MAX_ZOOM = 5f;
+    private float mPosX = 0f;
+    private float mPosY = 0f;
+
+    private float mLastTouchX;
+    private float mLastTouchY;
+    private static final int INVALID_POINTER_ID = -1;
+    private static final String LOG_TAG = "TouchImageView";
+
+    // The ‘active pointer’ is the one currently moving our object.
+    private int mActivePointerId = INVALID_POINTER_ID;
 
     private float scaleFactor = 1.f;
     private float pivotPointX = 0f;
@@ -30,15 +46,94 @@ public class CustomImageView extends ImageView {
     public CustomImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
         detector = new ScaleGestureDetector(getContext(), new ScaleListener());
-        //bitmap = ((BitmapDrawable)getDrawable()).getBitmap();
     }
-
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        detector.onTouchEvent(event);
+    public void setImageBitmap(Bitmap bm) {
+        super.setImageBitmap(bm);
+        bitmap = ((BitmapDrawable)getDrawable()).getBitmap();
+        drawable = getDrawable();
+        int bmWidth = bitmap.getWidth();
+        int bmHeight = bitmap.getHeight();
+        int drawableWidth = getWidth();
+        int drawableHeight = getHeight();
+        limitX = (bmWidth/2) - (drawableWidth/2);
+        limitY = (bmHeight/2) - (drawableHeight/2);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        detector.onTouchEvent(ev);
+
+        final int action = ev.getAction();
+        switch (action & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: {
+                final float x = ev.getX();
+                final float y = ev.getY();
+
+                mLastTouchX = x;
+                mLastTouchY = y;
+
+                mActivePointerId = ev.getPointerId(0);
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                final int pointerIndex = ev.findPointerIndex(mActivePointerId);
+                final float x = ev.getX(pointerIndex);
+                final float y = ev.getY(pointerIndex);
+
+                if (!detector.isInProgress()) {
+                    final float dx = x - mLastTouchX;
+                    final float dy = y - mLastTouchY;
+
+                    mPosX += dx;
+                    if (mPosX < (0 - limitX))
+                        mPosX = 0 - limitX;
+                    else if (mPosX > limitX)
+                        mPosX = limitX;
+                    mPosY += dy;
+                    if (mPosY < (0 - limitY))
+                        mPosY = 0 - limitY;
+                    else if (mPosY > limitY)
+                        mPosY = limitY;
+
+                    invalidate();
+                }
+
+                mLastTouchX = x;
+                mLastTouchY = y;
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                mActivePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_CANCEL: {
+                mActivePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_POINTER_UP: {
+                final int pointerIndex = (ev.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+                final int pointerId = ev.getPointerId(pointerIndex);
+                if (pointerId == mActivePointerId) {
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    mLastTouchX = ev.getX(newPointerIndex);
+                    mLastTouchY = ev.getY(newPointerIndex);
+                    mActivePointerId = ev.getPointerId(newPointerIndex);
+                }
+                break;
+            }
+        }
+
         return true;
     }
+
+
 
     @Override
     public void onDraw(Canvas canvas) {
@@ -46,13 +141,13 @@ public class CustomImageView extends ImageView {
 
         canvas.save();
 
+        canvas.translate(mPosX, mPosY);
+
         Matrix matrix = new Matrix();
         matrix.postScale(scaleFactor, scaleFactor, pivotPointX,
                 pivotPointY);
-        // canvas.setMatrix(matrix);
 
-        canvas.drawBitmap(
-                ((BitmapDrawable) this.getDrawable()).getBitmap(), matrix,
+        canvas.drawBitmap(bitmap, matrix,
                 null);
 
         canvas.restore();
